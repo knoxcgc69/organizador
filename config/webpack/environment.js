b1bd8139673aa6b10cffb33420c1c0b33eff7c4d
knox <knoxcgc69@gmail.com>
2023-07-28 21:26:25 -0500

    commit cocoon de mierda

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
+const webpack = requiere("webpack")
+
+Environment.plugins.prepend("Provide",
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+        Popper: ["popper.js", "default"]
+
+    }
+    )
+)
 
 module.exports = environment
